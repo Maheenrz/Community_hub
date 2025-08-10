f90a9119f8dfbc69e35cb0c987d86a0b40dddb60
Maheen Razzaq <mahheen508@gmail.com>
2025-08-10 20:10:26 +0000

    Initial commit for CommunityHub

--- communityhub/models.py
+++ communityhub/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+# one to one
 class Profile(models.Model):
     # Link profile to Django's built-in User model
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,13 +14,32 @@ class Profile(models.Model):
     def __str__(self):
         return self.display_name or self.user.username
 
-
-class Posts(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title= models.CharField(max_length=200)
-    content=models.TextField()
+# many to one
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=200)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
 
-    
+    class Meta:
+        ordering = ["-created_at"]
+
+# many to one
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at= models.DateTimeField(auto_now_add=True)
+    content= models.TextField()
+
+
+    def __str__(self):
+        return f'Commented by {self.author.username} on "{self.post.title}"'
+
+
+
+# many to one
+ 
